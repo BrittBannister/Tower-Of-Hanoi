9e3c0885945d6713df067a4aff44fca9419c0b95
Brittney Bannister <bbannister1127@icloud.com>
2020-06-25 10:36:30 -0400

    added winning condition, working on comparision for drop disc

--- README.md
+++ README.md
@@ -5,9 +5,14 @@
 1. Way to select disc from the top of tower
         1. attach a click handler to pick up top disc of each tower
                 tower.addEventListener('click',function)
-                tower.lastElementChild (to pick up the top disc only from each tower)
+                tower.lastElementChild 
+                        allows us to pick up the top disc only from each tower
+                When we click a tower, we are picking up a disk
+
+
 2. Way to move disc from tower to tower
         1. click handler on each tower - utilize like above ***
+                same ^^ to pick up && drop
         2. compare size of disc to drop vs top disc of current tower
             *Want to make sure we have the top disc. (tower.lastElChild)
                 a. if disc is smaller, add disc to top of current tower
@@ -15,6 +20,12 @@
                                 disc1.dataset.width < disc3.dataset.width
                 b. to add to current tower:
                         tower.appendChild(disc)
+        if statement.....if(disc)....if theres a disc then we fun the condition....if no disc, then we can go ahead and
+        drop the current disc. OH! We need a way to store the current disc....and maybe to store what position we are in? 
+                        let current = ?
+                        let mode? let disc =? <<<<Something like that. 
+
+
 3. Check for winning conditions
         1. all discs in tower 3
                 1. get number of disc in tower
@@ -22,3 +33,6 @@
                 2. possible if/else statement^^^
                 3. alert box "you won"? something fun!
 
+QUESTIONS:
+
+Do we need two functions? One for picking up and one for dropping? Or can we store those in a variable and just reference them throughout one function...in if/else statements or something.
--- main.js
+++ main.js
@@ -1,32 +1,48 @@
 let mode = 'pickup'
-let selected = null
+let selected = null //null because we dont have a disc selected when we first start the game
 
-const pickUpOrDropDisc = function(evt) {
+const pickUpOrDropDisc = function(evt) { //refactored this into one function, rather than using if for three seperate tower functions.
     const tower = evt.currentTarget
     if (mode === 'pickup') {
         const disc = tower.lastElementChild
-            //pickup
         if (disc) {
             console.log('you picked up disc ' + disc.id)
             selected = disc
+            pickedUp = selected
             mode = 'drop'
-
         } else {
             console.log('no disc picked up')
+        } ///////////////////////////////////////////
 
-        }
-    } else {
-        //drop
+    } else if (pickedUp.dataset.width < tower.lastElementChild.dataset.width) {
+        tower.appendChild(pickedUp)
+    }
+    /////////////////////////////////////////////
+    else { //drop
         console.log('you dropped the disc on ' + tower.id)
-        tower.append(selected)
+        tower.append(selected) //this will only work if we are in drop mode (selected)
         mode = 'pickup'
+    }
+    // let pickedUp = disc
+    // if (pickedUp.dataset.width > tower.lastElementChild.dataset.width) {
+    //     console.log('try again')
+    // } else {
+    //     tower.appendChild(pickedUp)
+    // }
+    winning();
+}
+
+
+function winning() {
+    if (tower1.lastElementChild === null && tower2.lastElementChild === null) { //if tower1 and tower2 are empty-all disc are in tower 3
+        console.log('you won')
+        alert('you won')
     }
 }
 
 const tower1 = document.querySelector('#tower1')
-const tower2 = document.querySelector('#tower2')
-const tower3 = document.querySelector('#tower3')
-
 tower1.addEventListener('click', pickUpOrDropDisc)
+const tower2 = document.querySelector('#tower2')
 tower2.addEventListener('click', pickUpOrDropDisc)
+const tower3 = document.querySelector('#tower3')
 tower3.addEventListener('click', pickUpOrDropDisc)
